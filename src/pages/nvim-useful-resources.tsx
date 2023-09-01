001e453a6fb348a7d96f2290372a2f2e4b7f17e2
Nikita Trofimov <n.v.trofimov@tinkoff.ru>
2023-09-02 01:53:29 +0300

    links

--- src/pages/nvim-useful-resources.tsx
+++ src/pages/nvim-useful-resources.tsx
@@ -11,6 +11,9 @@ const NvimUsefulResources = () => {
                 <li>
                     <a href='https://tylerneylon.com/a/learn-lua/' target='_blank' className='underline text-[#395B64] font-bold'>Lua in 15 minutes</a>
                 </li>
+                <li>
+                    <a href='https://hyperpolyglot.org/more' target='_blank' className='underline text-[#395B64] font-bold'>Hyperpolyglot - Сравнение синтаксиса языков</a>
+                </li>
             </ul>
 
             <span className='text-xl'>Tutorials and docs</span>
@@ -20,10 +23,63 @@ const NvimUsefulResources = () => {
                 </li>
             </ul>
 
+            <span className='text-xl'>Где искать плагины</span>
+            <ul className='px-2'>
+                <li>
+                    <a href='https://neovimcraft.com/' target='_blank' className='underline text-[#395B64] font-bold'>Neovimcraft</a>
+                </li>
+                <li>
+                    <a href='https://vimawesome.com/' target='_blank' className='underline text-[#395B64] font-bold'>Vimawesome</a>
+                </li>
+            </ul>
+            <span className='text-xl'>Список плагинов</span>
+            <ul className='px-2'>
+                <li>
+                    <a href='https://github.com/wbthomason/packer.nvim' target='_blank' className='underline text-[#395B64] font-bold'>Packer - Менеджер пакетов</a>
+                </li>
+                <li>
+                    <a href='https://github.com/nvim-tree/nvim-tree.lua' target='_blank' className='underline text-[#395B64] font-bold'>Nvim tree - file explorer</a>
+                </li>
+                <li>
+                    <a href='https://github.com/nvim-tree/nvim-web-devicons' target='_blank' className='underline text-[#395B64] font-bold'>Nvim web dev icons - иконки файлов и папок</a>
+                </li>
+                <li>
+                    <a href='https://github.com/nvim-lualine/lualine.nvim' target='_blank' className='underline text-[#395B64] font-bold'>Lualine - информационная строка</a>
+                </li>
+                <li>
+                    <a href='https://github.com/akinsho/bufferline.nvim' target='_blank' className='underline text-[#395B64] font-bold'>Bufferline - настройка табов для открытых файлов</a>
+                </li>
+                <li>
+                    <a href='https://github.com/nvim-telescope/telescope.nvim' target='_blank' className='underline text-[#395B64] font-bold'>Telescope - Поиск по файлам</a>
+                </li>
+                <li className='flex gap-2'>
+                    <a href='https://github.com/williamboman/mason.nvim' target='_blank' className='underline text-[#395B64] font-bold'>Mason</a>
+                    <a href='https://github.com/williamboman/mason-lspconfig.nvim' target='_blank' className='underline text-[#395B64] font-bold'>Mason lspconfig</a>
+                    <a href='https://github.com/neovim/nvim-lspconfig' target='_blank' className='underline text-[#395B64] font-bold'>Nvim lsp config</a>
+                    <span>(Для установки Language server protocol)</span>
+                </li>
+                <li>
+                    <a href='https://github.com/windwp/nvim-ts-autotag' target='_blank' className='underline text-[#395B64] font-bold'>nvim ts autotag - для автозакрытия тегов html</a>
+                </li>
+                <li>
+                    <a href='https://github.com/windwp/nvim-autopairs' target='_blank' className='underline text-[#395B64] font-bold'>Nvim autopairs - для закрытия скобок</a>
+                </li>
+                <li className='flex gap-2'>
+                    <a href='https://github.com/hrsh7th/nvim-cmp' target='_blank' className='underline text-[#395B64] font-bold'>nvim cmp</a>
+                    <a href='https://github.com/hrsh7th/cmp-nvim-lsp' target='_blank' className='underline text-[#395B64] font-bold'>cmp nvim lsp</a>
+                    <span>(Для автокомплита и документации интерфейсов)</span>
+                </li>
+            </ul>
             <span className='text-xl'>Готовые Конфигурации</span>
             <ul className='px-2'>
                 <li>
                     <a href='https://github.com/craftzdog/dotfiles-public' target='_blank' className='underline text-[#395B64] font-bold'>Craftzdog</a>
+                </li>
+                <li>
+                    <a href='https://github.com/LunarVim/LunarVim/tree/master' target='_blank' className='underline text-[#395B64] font-bold'>LunarVim</a>
+                </li>
+                <li>
+                    <a href='https://github.com/NvChad/NvChad' target='_blank' className='underline text-[#395B64] font-bold'>NvChad</a>
                 </li>
             </ul>
         </div>
